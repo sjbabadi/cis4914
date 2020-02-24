370d3c161f3ffdbeece8affbc93d29007ed3f081
sjbabadi <sheila.babadi@yahoo.com>
2020-02-24 12:53:29 -0500

    initial commit

---
+++ app/src/main/java/com/sjbabadi/cis4914/user_dash.java
@@ -0,0 +1,14 @@
+package com.sjbabadi.cis4914;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class user_dash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dash);
+    }
+}

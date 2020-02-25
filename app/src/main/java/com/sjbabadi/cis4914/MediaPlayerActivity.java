4a785945b2041431a4307389a04c262edd33b435
sjbabadi <sheila.babadi@yahoo.com>
2020-02-25 15:29:05 -0500

    added empty activities for user dash and media player

--- app/src/main/java/com/sjbabadi/cis4914/MediaPlayerActivity.java
+++ app/src/main/java/com/sjbabadi/cis4914/MediaPlayerActivity.java
@@ -1,14 +1,14 @@
 package com.sjbabadi.cis4914;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class user_dash extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MediaPlayerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_dash);
+        setContentView(R.layout.activity_media_player);
     }
 }
